730999b905a11298423f171193b15735a0c119d4
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-16 19:34:54 -0500

    can create list of doctors and select them

--- simpledoctorpage/src/App.jsx
+++ simpledoctorpage/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import doctorslist from './assets/doctors.json'
 
 function App() {
+  const [doctors] = useState(doctorslist)
+  const [selectedDoctor, setSelectedDoctor] = useState('')
 
+  const handleSelect = (doctor) => {
+    setSelectedDoctor(doctor)
+  }
 
   return (
     <>
-      
+      <ul>
+        {doctors.map((doctor, index) => (
+          <li key={index} onClick={() => handleSelect(doctor)}>
+            {doctor.Name} - {doctor.Specialty}
+          </li>
+        ))}
+      </ul>
+      {selectedDoctor && <div>Selected Doctor: {selectedDoctor.Name}</div>}
     </>
   )
 }
