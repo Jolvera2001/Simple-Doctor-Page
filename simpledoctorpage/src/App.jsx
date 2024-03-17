ccd1ea88fe73b2afb83a76f822dff55a9388e9ff
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-16 19:51:35 -0500

    Basic functionality finished

--- simpledoctorpage/src/App.jsx
+++ simpledoctorpage/src/App.jsx
@@ -4,10 +4,20 @@ import doctorslist from './assets/doctors.json'
 
 function App() {
   const [doctors] = useState(doctorslist)
-  const [selectedDoctor, setSelectedDoctor] = useState('')
+  const [selectedDoctor, setSelectedDoctor] = useState(null)
 
   const handleSelect = (doctor) => {
     setSelectedDoctor(doctor)
+  }
+
+  const getSimilarDoctors = () => {
+    if(!selectedDoctor) {
+      return []
+    }
+
+    return doctors.filter(
+      (doc) => doc.Area === selectedDoctor.Area && doc.Name !== selectedDoctor.Name
+    )
   }
 
   return (
@@ -19,7 +29,22 @@ function App() {
           </li>
         ))}
       </ul>
-      {selectedDoctor && <div>Selected Doctor: {selectedDoctor.Name}</div>}
+      {selectedDoctor && 
+        <div>
+          <h1>Name: {selectedDoctor.Name}</h1>
+          <h3>Specialty: {selectedDoctor.Specialty}</h3>
+          <h3>Area: {selectedDoctor.Area}</h3>
+          <h3>Review Score: {parseFloat(selectedDoctor.ReviewScore).toFixed(1)}</h3>
+          <h3>
+            Similar Doctors (Same Area):
+            {getSimilarDoctors().map((doctor, index) => (
+              <p key={index}>
+                {doctor.Name} - {doctor.Specialty}
+              </p>
+            ))}
+          </h3>
+        </div>
+      }
     </>
   )
 }
